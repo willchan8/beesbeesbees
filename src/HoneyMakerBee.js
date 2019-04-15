b4ba9cc3aa143b3131eec91d420ec9bf72859063
William Chan <wchan1992@gmail.com>
2019-04-15 12:15:12 -0700

    Implement HoneyMakerBee.js to pass all tests

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,22 @@
 var HoneyMakerBee = function() {
+  
+  Bee.call(this);
+
+  this.age = 10;
+  this.job = 'make honey';
+  this.honeyPot = 0;
+
 };
 
+// set the prototype
+HoneyMakerBee.prototype = Object.create(Grub.prototype);
+// set the constructor
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+// a makeHoney method that adds 1 to that honeyBee\'s honeyPot
+HoneyMakerBee.prototype.makeHoney = function () {
+  this.honeyPot++;
+}
+// a giveHoney method that subtracts 1 from that honeyBee\'s honeyPot
+HoneyMakerBee.prototype.giveHoney = function () {
+  this.honeyPot--;
+}
