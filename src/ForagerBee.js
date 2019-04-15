6773b895e519d392e6705dfe3a79b6c9d21575ab
William Chan <wchan1992@gmail.com>
2019-04-15 12:24:47 -0700

    Implement ForagerBee.js to pass all tests

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,18 @@
 var ForagerBee = function() {
+  // call the Bee superclass
+  Bee.call(this)
+  
+  this.age = 10;
+  this.job = 'find pollen';
+  this.canFly = true;
+  this.treasureChest = [];
 };
 
+// set the prototype
+ForagerBee.prototype = Object.create(Bee.prototype);
+// set the constructor
+ForagerBee.prototype.constructor = ForagerBee;
+// a forage method that allows the bee to add a treasure to the treasureChest
+ForagerBee.prototype.forage = function(treasure) {
+  this.treasureChest.push(treasure);
+}
