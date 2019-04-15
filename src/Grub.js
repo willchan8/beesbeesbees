84a80fbce0b884b802f2ba2ce3f6d37353c392ca
William Chan <wchan1992@gmail.com>
2019-04-15 12:13:40 -0700

    Implement Grub.js to pass all tests

--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,15 @@
 var Grub = function() {
+  // an age property that is set to 0
+  this.age = 0;
+
+  // a color property that is set to pink
+  this.color = 'pink';
+
+  // a food property that is set to jelly
+  this.food = 'jelly';
 };
 
+  // an eat method
+Grub.prototype.eat = function () {
+    
+}
